58b26ac62f7d39eb3e143f28edca57e545f122d7
AnjaniPatel-developer <anjanypatel@gmail.com>
2026-04-21 13:19:11 +0530

    second commit

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ ScrollReveal().reveal('.home-content p, .about-content ', { origin: 'right' });
 
 // typed js
  const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'YouTuber', 'Blogger'],
+    strings: ['MERN Stack Developer', 'YouTuber', 'Blogger'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
